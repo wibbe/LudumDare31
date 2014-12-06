f1ea2f569c706f4fd667633b1807a047baa2eabb
Daniel Wiberg <daniel.wiberg@dreambound.se>
2014-12-06 15:00:36 +0100

    Worked in the cells

--- game/scenes/fungus_cell.gd
+++ game/scenes/fungus_cell.gd
@@ -1,24 +1,42 @@
 
 extends Node2D
 
-const MAX_ENERGY = 100
-const INITIAL_ENERGY = 10
+const MAX_ENERGY = 100.0
+const INITIAL_ENERGY = 10.0
 
-var position = Vector2(0, 0)
-var energy = 0
-var game = null
+var pos = Vector2(0, 0)
+var current_energy = 0
+var board = null
 var sprite = null
-
-func set_game(g):
-	game = g
+var connections = []
 
 func _ready():
 	set_process(true)
-	energy = INITIAL_ENERGY
+	current_energy = INITIAL_ENERGY
 	sprite = get_node("Sprite")
 	
+	get_node("Selection").hide()
+	
 func _process(delta):
-	var scale = 0.4 + (energy / MAX_ENERGY) * 0.6
+	var scale = 0.4 + (current_energy / MAX_ENERGY) * 0.6
 	sprite.set_scale(Vector2(scale, scale))
 
+func initialize(pos_, board_):
+	pos = pos_
+	board = board_
 
+func tick():
+	if (current_energy < MAX_ENERGY):
+		var energy = board.draw_energy(pos)
+		current_energy += energy
+		print(current_energy)
+
+func select():
+	get_node("Selection").show()
+	
+func deselect():
+	get_node("Selection").hide()
+
+
+func is_player():
+	return true
